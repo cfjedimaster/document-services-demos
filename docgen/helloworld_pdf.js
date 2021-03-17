7323ffb6aaf3eac916fa6cfd2222dc76ad490584
Raymond Camden <raymondcamden@gmail.com>
2021-03-17 14:53:37 -0500

    more demo work

---
+++ docgen/helloworld_pdf.js
@@ -0,0 +1,45 @@
+const PDFToolsSdk = require('@adobe/documentservices-pdftools-node-sdk');
+const fs = require('fs');
+
+const inputFile = './hello1.docx';
+const outputFile = './helloWorld.pdf';
+
+const data = {
+    name: "Raymond Camden"
+};
+
+//remove output if exists
+if(fs.existsSync(outputFile)) fs.unlinkSync(outputFile);
+
+// Initial setup, create credentials instance.
+const credentials =  PDFToolsSdk.Credentials
+      .serviceAccountCredentialsBuilder()
+      .fromFile("pdftools-api-credentials.json")
+      .build();
+
+// Create an ExecutionContext using credentials.
+const executionContext = PDFToolsSdk.ExecutionContext.create(credentials);
+
+// Create a new DocumentMerge options instance.
+const documentMerge = PDFToolsSdk.DocumentMerge,
+      documentMergeOptions = documentMerge.options,
+      options = new documentMergeOptions.DocumentMergeOptions(data, documentMergeOptions.OutputFormat.PDF);
+
+// Create a new operation instance using the options instance.
+const documentMergeOperation = documentMerge.Operation.createNew(options);
+
+// Set operation input document template from a source file.
+const input = PDFToolsSdk.FileRef.createFromLocalFile(inputFile);
+documentMergeOperation.setInput(input);
+
+// Execute the operation and Save the result to the specified location.
+documentMergeOperation.execute(executionContext)
+.then(result => result.saveAsFile(outputFile))
+.catch(err => {
+    if(err instanceof PDFToolsSdk.Error.ServiceApiError
+        || err instanceof PDFToolsSdk.Error.ServiceUsageError) {
+        console.log('Exception encountered while executing operation', err);
+    } else {
+        console.log('Exception encountered while executing operation', err);
+    }
+});
--- docgen/readme.md
+++ docgen/readme.md
@@ -5,5 +5,7 @@ Main site: <https://www.adobe.io/apis/documentcloud/dcsdk/doc-generation.html>
 Note - these demos require your credentials in the same directory, both the JSON file and your private key.
 Note - docTemplate.docx makes use of a condition loop using greater than or equal. This is currently not evaluating correctly.
 
-test1.js - reads in docTemplate and outputs a Word doc
-test2.js - just a slightly nicer version, outputs to Word and PDF
+* test1.js - reads in docTemplate and outputs a Word doc
+* test2.js - just a slightly nicer version, outputs to Word and PDF
+* helloworld.js - simple example used for a blog post
+* helloword-pdf.js - a PDF version of the above
