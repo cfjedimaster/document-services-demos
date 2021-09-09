a45e628b6759b9ef9f08e2bdcd77d096f5074cea
Raymond Camden <raymondcamden@gmail.com>
2021-09-09 15:25:06 -0500

    misc

--- services/multi.js
+++ services/multi.js
@@ -9,7 +9,7 @@ result from one op to another! No need to save it. So this mean it's fairly triv
 This code is somehwat ugly now, but will be something I can demo nicer later.
 */
 
-const PDFToolsSdk = require('@adobe/documentservices-pdftools-node-sdk');
+const pdfSDK = require('@adobe/pdfservices-node-sdk');
 const fs = require('fs');
 
 //clean up previous
@@ -23,9 +23,9 @@ const fs = require('fs');
 	if(fs.existsSync(output)) fs.unlinkSync(output);
 
 	let result = await createPDF(input, creds);
-	console.log('got a result');
-	result = await ocrPDF(result, creds);
-	console.log('got second result');
+	console.log('got result from making a pdf');
+	result = await protectPDF(result, '12345', creds);
+	console.log('got result from password protecting pdf');
 
 	await result.saveAsFile(output);
 
@@ -35,27 +35,17 @@ async function createPDF(source, creds) {
 
     return new Promise((resolve, reject) => {
 
-		const credentials =  PDFToolsSdk.Credentials
+		const credentials =  pdfSDK.Credentials
 		.serviceAccountCredentialsBuilder()
 		.fromFile(creds)
 		.build();
 
-		const executionContext = PDFToolsSdk.ExecutionContext.create(credentials),
-				createPdfOperation = PDFToolsSdk.CreatePDF.Operation.createNew();
+		const executionContext = pdfSDK.ExecutionContext.create(credentials),
+				createPdfOperation = pdfSDK.CreatePDF.Operation.createNew();
 
 		// Set operation input from a source file
-		const input = PDFToolsSdk.FileRef.createFromLocalFile(source);
+		const input = pdfSDK.FileRef.createFromLocalFile(source);
 		createPdfOperation.setInput(input);
-
-		let stream = new Stream.Writable();
-		stream.write = function() {
-
-		}
-		
-		stream.end = function() {
-			console.log('end called');
-			resolve(stream);
-		}
 
 		// Execute the operation and Save the result to the specified location.
 		createPdfOperation.execute(executionContext)
@@ -76,23 +66,76 @@ async function ocrPDF(source, creds) {
 
     return new Promise((resolve, reject) => {
 
-		const credentials =  PDFToolsSdk.Credentials
+		const credentials =  pdfSDK.Credentials
 		.serviceAccountCredentialsBuilder()
 		.fromFile(creds)
 		.build();
 
-		const executionContext = PDFToolsSdk.ExecutionContext.create(credentials),
-  			ocrOperation = PDFToolsSdk.OCR.Operation.createNew();
+		const executionContext = pdfSDK.ExecutionContext.create(credentials),
+  			ocrOperation = pdfSDK.OCR.Operation.createNew();
+
+		// Set operation input from a source file.
+		ocrOperation.setInput(source);
+
+
+		// Execute the operation and Save the result to the specified location.
+		ocrOperation.execute(executionContext)
+       .then(result => resolve(result))
+       .catch(err => reject(err));
+
+	});
+}
+
+
+async function protectPDF(source, password, creds) {
+
+    return new Promise((resolve, reject) => {
+
+		const credentials =  pdfSDK.Credentials
+		.serviceAccountCredentialsBuilder()
+		.fromFile(creds)
+		.build();
+
+		const executionContext = pdfSDK.ExecutionContext.create(credentials);
+		const protectPDF = pdfSDK.ProtectPDF,
+			options = new protectPDF.options.PasswordProtectOptions.Builder()
+			.setUserPassword(password)
+			.setEncryptionAlgorithm(pdfSDK.ProtectPDF.options.EncryptionAlgorithm.AES_256)
+			.build();		
+
+		const protectPDFOperation = protectPDF.Operation.createNew(options);
+
+		// Set operation input from a source file.
+		protectPDFOperation.setInput(source);
+
+
+		// Execute the operation and Save the result to the specified location.
+		protectPDFOperation.execute(executionContext)
+       .then(result => resolve(result))
+       .catch(err => reject(err));
+
+	});
+}
+
+/*
+original demo had create/ocr, which doesnt make sense. Keeping this func cuz I like it
+*/
+async function ocrPDF(source, creds) {
+
+    return new Promise((resolve, reject) => {
+
+		const credentials =  pdfSDK.Credentials
+		.serviceAccountCredentialsBuilder()
+		.fromFile(creds)
+		.build();
+
+		const executionContext = pdfSDK.ExecutionContext.create(credentials),
+  			ocrOperation = pdfSDK.OCR.Operation.createNew();
 
 		// Set operation input from a source file.
 		//const input = PDFToolsSdk.FileRef.createFromStream(source);
 		ocrOperation.setInput(source);
 
-		let stream = new Stream.Writable();
-		stream.end = function() {
-			console.log('end called');
-			resolve(stream);
-		}
 
 		// Execute the operation and Save the result to the specified location.
 		ocrOperation.execute(executionContext)
