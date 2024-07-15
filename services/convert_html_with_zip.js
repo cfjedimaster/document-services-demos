97adf8fbf29b11f3a91d2d7ce48f3cf84d13c845
Raymond Camden <raymondcamden@gmail.com>
2024-07-15 13:56:07 -0500

    upd for new sdk

--- services/convert_html_with_zip.js
+++ services/convert_html_with_zip.js
@@ -7,7 +7,17 @@ require('dotenv').config();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-const PDFServicesSdk = require('@adobe/pdfservices-node-sdk');
+
+const {
+	ServicePrincipalCredentials,
+	PDFServices,
+	MimeType,
+	PageLayout,
+	HTMLToPDFParams, 
+	HTMLToPDFJob, 
+	HTMLToPDFResult
+} = require("@adobe/pdfservices-node-sdk");
+
 const AdmZip = require('adm-zip');
 const fs = require('fs');
 
@@ -21,65 +31,70 @@ let zip = new AdmZip();
 zip.addLocalFolder(inputDir);
 zip.writeZip('./temp.zip');
 
-const setCustomOptions = (htmlToPDFOperation) => {
-	// Define the page layout, in this case an 8 x 11.5 inch page (effectively portrait orientation).
-	const pageLayout = new PDFServicesSdk.CreatePDF.options.html.PageLayout();
-	pageLayout.setPageSize(8, 11.5);
-	//Set the dataToMerge field that needs to be populated in the HTML before its conversion.
-	const dataToMerge = {
-		"title":"Create, Convert PDFs and More!",
-		"sub_title": "Easily integrate PDF actions within your document workflows."
-	};
-	// Set the desired HTML-to-PDF conversion options.
-	const htmlToPdfOptions = new PDFServicesSdk.CreatePDF.options.html.CreatePDFFromHtmlOptions.Builder()
-		.includesHeaderFooter(true)
-		.withPageLayout(pageLayout)
-		.withDataToMerge(dataToMerge)
-		.build();
-	htmlToPDFOperation.setOptions(htmlToPdfOptions);
-};
+function getHTMLToPDFParams() {
+    // Define the page layout, in this case an 8 x 11.5 inch page (effectively portrait orientation)
+    const pageLayout = new PageLayout({
+        pageHeight: 11.5,
+        pageWidth: 8
+    });
 
+    // Set the dataToMerge field that needs to be populated in the HTML before its conversion
+    const dataToMerge = {
+        "title": "Create, Convert PDFs and More!",
+        "sub_title": "Easily integrate PDF actions within your document workflows."
+    };
 
-try {
-	// Initial setup, create credentials instance.
-	const credentials =  PDFServicesSdk.Credentials
-		.servicePrincipalCredentialsBuilder()
-		.withClientId(CLIENT_ID)
-		.withClientSecret(CLIENT_SECRET)
-		.build();
-
-	const clientConfig = PDFServicesSdk.ClientConfig
-		.clientConfigBuilder()
-		.withConnectTimeout(15000)
-		.withReadTimeout(15000)
-		.withProcessingTimeout(1200000)
-	.build();
-
-	// Create an ExecutionContext using credentials and create a new operation instance.
-	const executionContext = PDFServicesSdk.ExecutionContext.create(credentials, clientConfig), htmlToPDFOperation = PDFServicesSdk.CreatePDF.Operation.createNew();
-
-	// Set operation input from a source file.
-	const input = PDFServicesSdk.FileRef.createFromLocalFile('./temp.zip');
-	htmlToPDFOperation.setInput(input);
-
-	// Provide any custom configuration options for the operation.
-	setCustomOptions(htmlToPDFOperation);
-
-	// Execute the operation and Save the result to the specified location.
-	htmlToPDFOperation.execute(executionContext)
-		.then(result => result.saveAsFile('output/createPdfFromDynamicHtmlOutput.pdf'))
-		.then(() => {
-			// clean up after myself
-			fs.unlinkSync('./temp.zip');
-		})
-		.catch(err => {
-			if(err instanceof PDFServicesSdk.Error.ServiceApiError
-				|| err instanceof PDFServicesSdk.Error.ServiceUsageError) {
-				console.log('Exception encountered while executing operation', err);
-			} else {
-				console.log('Exception encountered while executing operation', err);
-			}
-		});
-} catch (err) {
-console.log('Exception encountered while executing operation', err);
+    return new HTMLToPDFParams({
+        pageLayout,
+        dataToMerge,
+        includeHeaderFooter: true,
+    });
 }
+
+(async () => {
+
+	try {
+		// Initial setup, create credentials instance.
+		const credentials = new ServicePrincipalCredentials({
+			clientId: CLIENT_ID,
+			clientSecret: CLIENT_SECRET
+		});
+
+		const pdfServices = new PDFServices({credentials});
+
+		let readStream = fs.createReadStream("./temp.zip");
+		const inputAsset = await pdfServices.upload({
+			readStream,
+			mimeType: MimeType.ZIP
+		});
+
+		const params = new getHTMLToPDFParams();
+		const job = new HTMLToPDFJob({inputAsset, params });
+
+		const pollingURL = await pdfServices.submit({job});
+        const pdfServicesResponse = await pdfServices.getJobResult({
+            pollingURL,
+            resultType: HTMLToPDFResult
+        });
+
+
+		const resultAsset = pdfServicesResponse.result.asset;
+        const streamAsset = await pdfServices.getContent({asset: resultAsset});
+
+        // Creates an output stream and copy result asset's content to it
+        const outputFilePath = 'output/createPdfFromDynamicHtmlOutput.pdf';
+
+        console.log(`Saving asset at ${outputFilePath}`);
+
+		const outputStream = fs.createWriteStream(outputFilePath);
+        streamAsset.readStream.pipe(outputStream);
+		
+		fs.unlinkSync('./temp.zip');
+
+		console.log('Done');
+	
+	} catch (err) {
+	console.log('Exception encountered while executing operation', err);
+	}
+
+})();
